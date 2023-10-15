50e284cdf665526a8718c1b0493ff3a11ba2bffc
Mahmoud <mramadan.comm@yahoo.com>
2023-10-14 19:26:19 -0700

    remove duplicate keys and add more cleanup

--- script.js
+++ script.js
@@ -417,85 +417,83 @@ const arabicWords = ["حرق", " الحرق","القتل","إسرائيل","ال
 
 
 const arabicReplacements = {
-"س": "ـ,s,ـ",
-"ص": "ـ,s,ـ",
-"ظ": "ـ,z,ـ",
-"ذ": "ـ,z,ـ",
-"ذ": "ـ,z,ـ",
-"ز": "ـ,z,ـ",
-"ك": "ـ,k,ـ",
-"ه": "ـ,h,ـ",
-"ط": "ـ,t,ـ",
-"ت": "ـ,t,ـ",
-"ن": "ـ,n,ـ",
-"د": "ـ,d,ـ",
-"و": "ـ,w,ـ",
-"ج": "ـ,g,ـ",
-"ب": "ـ,b,ـ",
- "ر":"ـ,r,ـ",
- "ت":"ـ,t,ـ",
- "ث":"ـ,s,ـ",
- "ض":"ـ,d,ـ",
- "ف":"ـ,f,ـ",
-"ق":"ـ,k,ـ",
-"ل":"ـ,l,ـ",
-"م":"ـ,m,ـ"
+  "س": "ـ,s,ـ",
+  "ص": "ـ,s,ـ",
+  "ظ": "ـ,z,ـ",
+  "ذ": "ـ,z,ـ",
+  "ز": "ـ,z,ـ",
+  "ك": "ـ,k,ـ",
+  "ه": "ـ,h,ـ",
+  "ط": "ـ,t,ـ",
+  "ت": "ـ,t,ـ",
+  "ن": "ـ,n,ـ",
+  "د": "ـ,d,ـ",
+  "و": "ـ,w,ـ",
+  "ج": "ـ,g,ـ",
+  "ب": "ـ,b,ـ",
+  "ر": "ـ,r,ـ",
+  "ث": "ـ,s,ـ",
+  "ض": "ـ,d,ـ",
+  "ف": "ـ,f,ـ",
+  "ق": "ـ,k,ـ",
+  "ل": "ـ,l,ـ",
+  "م": "ـ,m,ـ"
 };
 
-function modifyWord (word) {
-let modifiedWord = word.split('');
-let found = false;
 
-for (let i = 0; i < word.length; i++) {
-  let char = word[i];
-  if (arabicReplacements[char] && !found) {
-    modifiedWord[i] = arabicReplacements[char];
-    found = true;
+function modifyWord(word) {
+  let modifiedWord = word.split('');
+  let found = false;
+
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+    if (arabicReplacements[char] && !found) {
+      modifiedWord[i] = arabicReplacements[char];
+      found = true;
+    }
   }
+
+  return modifiedWord.join('');
 }
 
-return modifiedWord.join('');
-}
+function modifyText(inputText) {
+  let words = inputText.split(' ');
+  let modifiedText = [];
 
-function modifyText (inputText) {
-let words = inputText.split(' ');
-let modifiedText = [];
+  words.forEach(word => {
+    if (arabicWords.includes(word)) {
+      modifiedText.push(modifyWord(word));
+    } else {
+      modifiedText.push(word);
+    }
+  });
 
-words.forEach(word => {
-  if (arabicWords.includes(word)) {
-    modifiedText.push(modifyWord(word));
-  } else {
-    modifiedText.push(word);
-  }
-});
-
-return modifiedText.join(' ');
+  return modifiedText.join(' ');
 }
 
 const form = document.getElementById('text-form');
 const inputText = document.getElementById('input-text');
 const outputText = document.getElementById('output-text');
 
-form.addEventListener('keyup',
-e => {
-e.preventDefault();
+form.addEventListener('keyup', (e) => {
+  e.preventDefault();
 
-const text = inputText.value;
-const modified = modifyText(text);
+  const text = inputText.value;
+  const modified = modifyText(text);
 
-outputText.textContent = modified;
+  outputText.textContent = modified;
 });
 
 const copyButton = document.getElementById('copy-button');
 
-copyButton.addEventListener('click',
-() => {
-let text = document.getElementById('output-text').textContent;
+copyButton.addEventListener('click', () => {
+  let text = outputText.textContent;
 
-if (navigator.clipboard && navigator.clipboard.writeText) {
-  navigator.clipboard.writeText(text);
-  alert('تم نسخ النص بنجاح, النصر و العزة لفلسطين!');
-} else {
-  alert('Cannot copy text to clipboard. Please copy the text manually.');
-}
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    navigator.clipboard.writeText(text).then(() => {
+      alert('تم نسخ النص بنجاح, النصر و العزة لفلسطين!');
+    }).catch(() => {
+      alert('Cannot copy text to clipboard. Please copy the text manually.');
+    });
+  }
 });
