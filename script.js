e187f6f359781b51ba2ce076a9b73c2a5ff5c853
Mahmoud <mramadan.comm@yahoo.com>
2023-10-14 21:20:43 -0700

    the core of new random algorithm

--- script.js
+++ script.js
@@ -50,6 +50,52 @@ const arabicReplacements = {
   "م": "ـ,m,ـ"
 };
 
+// The core of the script and data hiding algorithm
+function getRandomCharacterSet() {
+  const characterSets = ["+++++++++++++", "ــــــــــــــ", "٠٠٠٠٠٠٠٠٠٠٠٠٠٠", "ّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّّ" , "ءءءءءءءءءءءءء"];
+  const randomIndex = Math.floor(Math.random() * characterSets.length);
+  return characterSets[randomIndex];
+}
+
+function generateRandomString(length, charset) {
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    randomString += charset.charAt(randomIndex);
+  }
+
+  return randomString;
+}
+
+function modifyWord2(word) {
+  charset = getRandomCharacterSet();
+  // Check if the word has less than 3 characters; if so, no modification is needed
+  if (word.length < 3) {
+    return word;
+  }
+
+  // Extract the first and last characters
+  const firstChar = word.charAt(0);
+  const lastChar = word.charAt(word.length - 1);
+
+  // Modify characters in the middle by appending random strings
+  let middleChars = '';
+
+  for (let i = 1; i < word.length - 1; i++) {
+    const originalChar = word.charAt(i);
+    const randomLength = (Math.floor(Math.random() * 5) + 1)%3; // Generate a random length (1 to 5 characters)
+    const randomString = "ــ"+generateRandomString(randomLength, charset)+"ـ";
+    middleChars += originalChar + randomString;
+  }
+
+  // Combine the first, modified middle, and last characters
+  const encodedWord = firstChar + middleChars + lastChar;
+
+  return encodedWord;
+}
+
+
 
 function modifyWord(word) {
   let modifiedWord = word.split('');
@@ -72,7 +118,7 @@ function modifyText(inputText) {
 
   words.forEach(word => {
     if (arabicWords.includes(word)) {
-      modifiedText.push(modifyWord(word));
+      modifiedText.push(modifyWord2(word));
     } else {
       modifiedText.push(word);
     }
